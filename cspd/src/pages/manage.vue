b83ca935f01b3ecfd38132b8da8b26bf3686561c
zhangzhaohuang <firebrilliant@dingtalk.com>
2018-06-29 09:09:30 +0800

    医生端首次上传

--- cspd/src/pages/manage.vue
+++ cspd/src/pages/manage.vue
@@ -1,21 +1,24 @@
 <template>
   <div class="manage_page fillcontain">
     <el-row style="height: 100%;">
-      <el-col :span="4"  style="min-height: 100%; background-color: #324057;">
-        <el-menu :default-active="defaultActive" style="min-height: 100%;" theme="dark" router>
-          <el-submenu index="1">
+      <el-col :span="4" style="min-height: 100%; background-color: #324057;">
+        <el-menu
+          :default-active="defaultActive"
+          style="min-height: 100%;"
+          router>
+          <el-menu-item index="manage"><i class="el-icon-menu"></i>首页</el-menu-item>
+          <el-submenu index="2">
             <template slot="title"><i class="el-icon-document"></i>个人中心</template>
             <el-menu-item index="manage">个人账户</el-menu-item>
             <el-menu-item index="msg">消息列表</el-menu-item>
           </el-submenu>
-          <el-submenu index="2">
+          <el-submenu index="3">
             <template slot="title"><i class="el-icon-plus"></i>服务管理</template>
             <el-menu-item index="tel_consult">电话咨询</el-menu-item>
             <el-menu-item index="imgText_consult">图文咨询</el-menu-item>
             <el-menu-item index="indetification">认证详情</el-menu-item>
             <el-menu-item index="server_setting">服务设置</el-menu-item>
           </el-submenu>
-
         </el-menu>
       </el-col>
       <el-col :span="20" style="height: 100%;overflow: auto;">
@@ -28,79 +31,74 @@
 </template>
 
 <script>
+  export default {
+    data: function () {
+      return {
+        nav: [
+          {
+            itemText: "个人中心",
+            childLists: [
+              {
+                text: "个人账户",
+                url: "/manage",
+                icon: ''
+              }, {
+                text: "消息列表",
+                url: "/manage/msg",
+                icon: ''
+              },
+            ]
+          }, {
+            itemText: "服务管理",
+            childLists: [
+              {
+                text: "电话咨询",
+                url: "/manage/tel_consult",
+                icon: ''
+              }, {
+                text: "图文咨询",
+                url: "/manage/imgText_consult",
+                icon: ''
+              }, {
+                text: "认证详情",
+                url: "/manage/indetification",
+                icon: ''
+              }, {
+                text: "服务设置",
+                url: "/manage/server_setting",
+                icon: ''
+              },
+            ]
+          },
 
 
+        ]
+      }
+    },
+    computed: {
+      defaultActive: function () {
+        return this.$route.path.replace('/', '');
+      }
+    },
 
-
-
-
-    export default {
-      data:function () {
-        return{
-          nav:[
-            {
-              itemText:"个人中心",
-              childLists:[
-                {
-                  text:"个人账户",
-                  url:"/manage",
-                  icon:''
-                },{
-                  text:"消息列表",
-                  url:"/manage/msg",
-                  icon:''
-                },
-              ]
-            },{
-              itemText:"服务管理",
-              childLists:[
-                {
-                  text:"电话咨询",
-                  url:"/manage/tel_consult",
-                  icon:''
-                },{
-                  text:"图文咨询",
-                  url:"/manage/imgText_consult",
-                  icon:''
-                },{
-                  text:"认证详情",
-                  url:"/manage/indetification",
-                  icon:''
-                },{
-                  text:"服务设置",
-                  url:"/manage/server_setting",
-                  icon:''
-                },
-              ]
-            },
-
-
-          ]
-        }
-      },
-      computed: {
-        defaultActive: function(){
-          return this.$route.path.replace('/', '');
-        }
-      },
-
-      methods:{
-        //点击nav执行方法
-        jumpLink:function (item) {
-          this.$router.push(item)
-        }
+    methods: {
+      //点击nav执行方法
+      jumpLink: function (item) {
+        this.$router.push(item)
       }
     }
+  }
 
 </script>
 
 <style scoped>
 
 
-  .full-page{
+  .full-page {
     height: 100%;
   }
-  .el-header{
+
+  .el-header {
     background-color: #B3C0D1;
     color: #333;
   }
